b120ed6b4fc1e3db97ed425047032fd9f2009cfa
Yana-Romanenko <yanaromanenko@ukr.net>
2023-12-10 16:34:58 +0200

    updated hw

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,27 @@
+'use strict'
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Напиши скрипт, який змінює колір фону елемента
+//   < body > через інлайн - стиль по кліку на button.change - color 
+//   і задає це значення 
+// кольору текстовим вмістом для span.color.
+
+const color = document.querySelector('.color');
+const changingColor = document.querySelector('.change-color');
+
+const clickButton = () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+};
+
+changingColor.addEventListener('click', clickButton);
+
+
--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,48 @@
+'use strict'
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const inputEl = document.querySelector('input');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputEl.value;
+  
+  if (amount >= 1 && amount <= 100) {
+    const boxes = [];
+
+    for (let i = 0; i < amount; i += 1) {
+      const box = document.createElement('div');
+      const size = 30 + i * 10;
+      const color = getRandomHexColor();
+
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = color;
+
+      boxes.push(box);
+    }
+    
+    boxesEl.append(...boxes);
+
+    inputEl.value = '';
+  } else {
+    console.log('Invalid input value. Please enter a number between 1 and 100.');
+  }
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+
